17df9b344e5b156218558a001a378cf8c3ad2abf
Norihiro Kamae <norihiro@nagater.net>
2024-11-06 11:14:08 +0900

    Generated by vst3projectgenerator
    cmake \
        -D SMTG_VENDOR_NAME_CLI="Nagater Networks" \
        -D SMTG_VENDOR_HOMEPAGE_CLI="https://www.nagater.net/" \
        -D SMTG_VENDOR_EMAIL_CLI="norihiro@nagater.net" \
        -D SMTG_PLUGIN_NAME_CLI="VBAN" \
        -D SMTG_PREFIX_FOR_FILENAMES_CLI="vban_" \
        -D SMTG_PLUGIN_IDENTIFIER_CLI="net.nagater.vst3.vban" \
        -D SMTG_CMAKE_PROJECT_NAME_CLI="VBANPlugin" \
        -D SMTG_VENDOR_NAMESPACE_CLI="NagaterNet" \
        -D SMTG_PLUGIN_CLASS_NAME_CLI="CVBANPlugin" \
        -D SMTG_PLUGIN_CATEGORY_CLI="Fx" \
        -D SMTG_PLUGIN_BUNDLE_NAME_CLI="vbanpluginbundle" \
        -D SMTG_VST3_SDK_SOURCE_DIR_CLI="" \
        -D SMTG_GENERATOR_OUTPUT_DIRECTORY_CLI="" \
        -D SMTG_ENABLE_VSTGUI_SUPPORT_CLI=OFF \
        -P GenerateVST3Plugin.cmake

---
+++ source/vban_controller.cpp
@@ -0,0 +1,105 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2024 Nagater Networks.
+//------------------------------------------------------------------------
+
+#include "vban_controller.h"
+#include "vban_cids.h"
+
+
+using namespace Steinberg;
+
+namespace NagaterNet {
+
+//------------------------------------------------------------------------
+// CVBANPluginController Implementation
+//------------------------------------------------------------------------
+tresult PLUGIN_API CVBANPluginController::initialize (FUnknown* context)
+{
+	// Here the Plug-in will be instantiated
+
+	//---do not forget to call parent ------
+	tresult result = EditControllerEx1::initialize (context);
+	if (result != kResultOk)
+	{
+		return result;
+	}
+
+	// Here you could register some parameters
+
+	return result;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API CVBANPluginController::terminate ()
+{
+	// Here the Plug-in will be de-instantiated, last possibility to remove some memory!
+
+	//---do not forget to call parent ------
+	return EditControllerEx1::terminate ();
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API CVBANPluginController::setComponentState (IBStream* state)
+{
+	// Here you get the state of the component (Processor part)
+	if (!state)
+		return kResultFalse;
+
+	return kResultOk;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API CVBANPluginController::setState (IBStream* state)
+{
+	// Here you get the state of the controller
+
+	return kResultTrue;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API CVBANPluginController::getState (IBStream* state)
+{
+	// Here you are asked to deliver the state of the controller (if needed)
+	// Note: the real state of your plug-in is saved in the processor
+
+	return kResultTrue;
+}
+
+//------------------------------------------------------------------------
+IPlugView* PLUGIN_API CVBANPluginController::createView (FIDString name)
+{
+	// Here the Host wants to open your editor (if you have one)
+	if (FIDStringsEqual (name, Vst::ViewType::kEditor))
+	{
+		// create your editor here and return a IPlugView ptr of it
+        return nullptr;
+	}
+	return nullptr;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API CVBANPluginController::setParamNormalized (Vst::ParamID tag, Vst::ParamValue value)
+{
+	// called by host to update your parameters
+	tresult result = EditControllerEx1::setParamNormalized (tag, value);
+	return result;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API CVBANPluginController::getParamStringByValue (Vst::ParamID tag, Vst::ParamValue valueNormalized, Vst::String128 string)
+{
+	// called by host to get a string for given normalized value of a specific parameter
+	// (without having to set the value!)
+	return EditControllerEx1::getParamStringByValue (tag, valueNormalized, string);
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API CVBANPluginController::getParamValueByString (Vst::ParamID tag, Vst::TChar* string, Vst::ParamValue& valueNormalized)
+{
+	// called by host to get a normalized value from a string representation of a specific parameter
+	// (without having to set the value!)
+	return EditControllerEx1::getParamValueByString (tag, string, valueNormalized);
+}
+
+//------------------------------------------------------------------------
+} // namespace NagaterNet
---
+++ source/vban_entry.cpp
@@ -0,0 +1,53 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2024 Nagater Networks.
+//------------------------------------------------------------------------
+
+#include "vban_processor.h"
+#include "vban_controller.h"
+#include "vban_cids.h"
+#include "version.h"
+
+#include "public.sdk/source/main/pluginfactory.h"
+
+#define stringPluginName "VBAN"
+
+using namespace Steinberg::Vst;
+using namespace NagaterNet;
+
+//------------------------------------------------------------------------
+//  VST Plug-in Entry
+//------------------------------------------------------------------------
+// Windows: do not forget to include a .def file in your project to export
+// GetPluginFactory function!
+//------------------------------------------------------------------------
+
+BEGIN_FACTORY_DEF ("Nagater Networks", 
+			       "https://www.nagater.net/", 
+			       "mailto:norihiro@nagater.net")
+
+	//---First Plug-in included in this factory-------
+	// its kVstAudioEffectClass component
+	DEF_CLASS2 (INLINE_UID_FROM_FUID(kCVBANPluginProcessorUID),
+				PClassInfo::kManyInstances,	// cardinality
+				kVstAudioEffectClass,	// the component category (do not changed this)
+				stringPluginName,		// here the Plug-in name (to be changed)
+				Vst::kDistributable,	// means that component and controller could be distributed on different computers
+				CVBANPluginVST3Category, // Subcategory for this Plug-in (to be changed)
+				FULL_VERSION_STR,		// Plug-in version (to be changed)
+				kVstVersionString,		// the VST 3 SDK version (do not changed this, use always this define)
+				CVBANPluginProcessor::createInstance)	// function pointer called when this component should be instantiated
+
+	// its kVstComponentControllerClass component
+	DEF_CLASS2 (INLINE_UID_FROM_FUID (kCVBANPluginControllerUID),
+				PClassInfo::kManyInstances, // cardinality
+				kVstComponentControllerClass,// the Controller category (do not changed this)
+				stringPluginName "Controller",	// controller name (could be the same than component name)
+				0,						// not used here
+				"",						// not used here
+				FULL_VERSION_STR,		// Plug-in version (to be changed)
+				kVstVersionString,		// the VST 3 SDK version (do not changed this, use always this define)
+				CVBANPluginController::createInstance)// function pointer called when this component should be instantiated
+
+	//----for others Plug-ins contained in this factory, put like for the first Plug-in different DEF_CLASS2---
+
+END_FACTORY
